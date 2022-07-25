42355dd8f08569145a6bb53c3b55fd3e7025a4e2
Rpatel2303 <109520311+Rpatel2303@users.noreply.github.com>
2022-07-25 15:58:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,10 +23,62 @@ console.log(description);
 
 const newDescription = `${country} is in ${continent} and its 11 million people speak portuguese`;
 console.log(newDescription);
-
-let BMI = 23.9;
+const massWeightRahul = 23.9;
+const massWeightPatel = 28.3;
+let BMI;
 if (BMI <= 25) {
   console.log("Mark's BMI is higher than John's");
 } else {
   console.log("John's BMI is higher than Mark's");
 }
+
+let a = '19';
+let b = '13';
+let c = 17;
+
+console.log('19' - '17' + '1');
+
+const numNeighbour = Number(prompt('How many neighbour countries does your country have?'));
+if (numNeighbour === 1) {
+  console.log('Only1border!');
+} else if (numNeighbour >= 1) {
+  console.log('Morethan1border');
+} else
+  console.log('no borders !');
+
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log("Dolphins wins the trophy !");
+}
+else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log("Koalas wins the trophy !");
+}
+else if (scoreDolphins || scoreKoalas > 100) {
+  console.log("Both of them wins the trophy !!");
+}
+else {
+  console.log("no one wins");
+}
+
+
+const day = 'wednesday';
+
+if (day === 'monday') {
+  console.log("plan course & go to coading meetup");
+}
+else if (day === 'tuesday') {
+  console.log("prepare videos");
+}
+else if (day === 'wednesday' || day === 'thursday') {
+  console.log("write codes");
+}
+else {
+  console.log("Not a valid day!!! ");
+}
+const totBill = 40;
+const tip = totBill >= 50 && totBill <= 300 ? totBill * 0.20 : totBill * 0.15;
+console.log(`the value is ${tip}`);
+console.log(`the total bill was ${totBill} and tip is ${tip} so the value is ${totBill + tip}`);
